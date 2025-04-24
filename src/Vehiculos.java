e6084521b55913e1caca13d87bfda013dfe19091
POO <simbanaalexis2004@gmail.com>
2025-04-24 09:27:45 -0500

    Atributos

--- src/Vehiculos.java
+++ src/Vehiculos.java
@@ -1,8 +1,23 @@
 public class Vehiculos {
-
     //4 atributos
     //2 metodos
     //3 objetos
+
+    //Atributos
+    String modelo;
+    String color;
+    String placa;
+    double velocidad_max;
+
+    // constructor
+    public Vehiculos(String modelo, String color, String placa, double velocidad_max) {
+
+        this.modelo = modelo;
+        this.color = color;
+        this.placa = placa;
+        this.velocidad_max = velocidad_max;
+    }
+
 
 
 }
