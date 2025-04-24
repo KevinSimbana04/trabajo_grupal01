f60e4b562a556cc5de90a11cd5f6271045c0e929
POO <simbanaalexis2004@gmail.com>
2025-04-24 09:36:35 -0500

    Atributos, metodos

--- src/Vehiculos.java
+++ src/Vehiculos.java
@@ -18,6 +18,20 @@ public class Vehiculos {
         this.velocidad_max = velocidad_max;
     }
 
+    // Accion 1 , visualizacion de datos del  vehiculo
 
+    public void mostrar_vehiculo(){
+        System.out.println("Datos del Vehiculo \n");
+        System.out.println("--------------------------\n");
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Color: " + color);
+        System.out.println("Placa: " + placa);
+        System.out.println("Velocidad max: " + velocidad_max);
+    }
+
+    // Accion 2 ,
+    public void velocidad_velocidad(){
+
+    }
 
 }
